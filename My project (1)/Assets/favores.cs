5b954bab3de508292f1fe47ca4a38bb0139f40e0
diegosuaste <123321249+diegosuaste@users.noreply.github.com>
2023-03-02 13:13:05 -0600

    examen_diego_antonio_suaste_garcia

---
+++ My project (1)/Assets/favores.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class favores : MonoBehaviour
+
+   
+  
+
+
+{
+    // Start is called before the first frame update
+    void botonesencontra()
+    {
+
+
+       
+
+
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    void botonesafavor()
+    {
+
+    }
+
+
+
+
+
+
+
+}
